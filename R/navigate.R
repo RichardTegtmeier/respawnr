bc8879b18f19db36172c3bf8a18cef7e14f24647
RichardTegtmeier <140663941+RichardTegtmeier@users.noreply.github.com>
2026-01-14 00:07:44 -0500

    Update navigate.R

--- R/navigate.R
+++ R/navigate.R
@@ -22,3 +22,33 @@ jump_to_marker <- function(name, offset = 5) {
     column = 1
   )
 }
+
+jump_next_marker <- function() {
+  ctx <- rstudioapi::getActiveDocumentContext()
+  cursor <- ctx$selection[[1]]$range$start["row"] + 1
+  markers <- find_markers()
+
+  next_hit <- markers[markers$line > cursor, ]
+  if (nrow(next_hit) == 0) return(invisible())
+
+  rstudioapi::setCursorPosition(
+    documentId = ctx$id,
+    row = next_hit$line[1],
+    column = 1
+  )
+}
+
+jump_prev_marker <- function() {
+  ctx <- rstudioapi::getActiveDocumentContext()
+  cursor <- ctx$selection[[1]]$range$start["row"] + 1
+  markers <- find_markers()
+
+  prev_hit <- markers[markers$line < cursor, ]
+  if (nrow(prev_hit) == 0) return(invisible())
+
+  rstudioapi::setCursorPosition(
+    documentId = ctx$id,
+    row = tail(prev_hit$line, 1),
+    column = 1
+  )
+}
