28048ebe4eae5107a0df62a5ca46b0a7374dded4
RichardTegtmeier <140663941+RichardTegtmeier@users.noreply.github.com>
2026-01-13 22:16:43 -0500

    Update addins.R

--- R/addins.R
+++ R/addins.R
@@ -1,1 +1,17 @@
+pick_marker <- function() {
+  markers <- find_markers()
 
+  if (nrow(markers) == 0) {
+    rstudioapi::showDialog("respawnr", "No markers found.")
+    return(invisible())
+  }
+
+  choice <- rstudioapi::selectList(
+    choices = markers$name,
+    title = "Jump to respawn marker"
+  )
+
+  if (!is.null(choice)) {
+    jump_to_marker(choice)
+  }
+}
